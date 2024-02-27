bcfea979699cd852790797e462e67145b502a704
Mathias Christiansen <Smatten2264@outlook.com>
2024-02-27 12:54:51 +0100

    main, oprette en class

--- character.py
+++ character.py
@@ -1,8 +1,8 @@
 class character:
-    def __init__(self, ID, name, type, gender, race, RPGclass):
+    def __init__(self, ID, name, race, RPGclass):
         self.ID = ID
         self.name = name
-        self.type = type
-        self.gender = gender
+        #self.type = type
+        #self.gender = gender
         self.race = race
         self.RPGclass = RPGclass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mapRenderer
 import os
+import character
 
 # mapRenderer.mapPainter()
 
@@ -10,14 +11,38 @@ three distinct races coexist – Humans, Elves, and Dwarves. For centuries.
 They have shared the blessings of their fertile lands, harnessed the power of arcane forces, and built great cities that touched the skies.
 """
 
+
+
+def startAnswer(answer):
+    accpetableAnswer = ["yes"]
+    while start.lower() not in accpetableAnswer:
+        print("You have to be ready")
+    start = input("are you ready for an adventure?")
+
+class Character:
+    def __init__ (self, name, race, RPGClass):
+        self.name = name
+        self.race = race
+        self.RPGClass = RPGClass
+
+
+def __str(self):
+    return (f"Charecter: {self.name}\nRace: {self.race}\nRPGClass: {self.RPGClass}")
+
+def create_Character():
+    print("Welcome to Character creation!")
+    name = input("Enter your name: ")
+    race = input(f"Enter your race: [Dwarf, Human or Elf] ")
+    RPGClass = input("Enter your Class: [Warrior, Mage or Artcher] ")
+
+    player = character.character("player", name, race, RPGClass)
+    return player
+
 print(text)
 
 name = input("What is your name? ")
 print(f"Nice to meeet you {name}")
 start = input("Are you ready for the adventure if your life? ")
 
-def startCon(start):
-    accpetableAnswer = ["ja"]
-    while start.lower() not in accpetableAnswer:
-        print("You have to be ready")
-    start = input("are you ready for an adventure?")
+player = create_Character()
+
