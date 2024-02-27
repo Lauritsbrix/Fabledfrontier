1e1613b99b456b21ba357e2a22f0149b0e87c422
poro246 <seb.zzxzz@gmail.com>
2024-02-27 11:13:04 +0100

    race start

---
+++ Race.py
@@ -0,0 +1,14 @@
+race_info = {
+    'Human': {
+        'description': 'A simple and ordinary person, decent at most things but that is all',
+        'stats': {'vitality': 1, 'strength': 1, 'magic': 1, 'agility': 1},
+    },
+    'Elf': {
+        'description': 'A mystical folk from the deep forest, has great magical attributes and are highly adept.',
+        'stats': {'vitality': 0, 'strength': 0, 'magic': 2, 'agility': 2},
+    },
+    'Dwarf': {
+        'description': 'Warriors are brave fighters adept with heavy weapons and armor. They have high health and strength.',
+        'stats': {'vitality': 2, 'strength': 2, 'magic': 0, 'agility': 0},
+    },
+}
