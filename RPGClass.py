83df4745e5eac64fcc4557f87ade1d07cef7b286
Mathias Christiansen <Smatten2264@outlook.com>
2024-02-27 12:02:25 +0100

    RPGClass

---
+++ RPGClass.py
@@ -0,0 +1,11 @@
+class RPGCClass:
+    def __init__(self):
+        self.description= ""
+        self.vitMod= 0
+        self.strMod= 0
+        self.magMod= 0
+        self.agiMod= 0
+
+mage = RPGCClass()
+warrior = RPGCClass()
+Archer = RPGCClass()
