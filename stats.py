70c6a6fb37e988af15f079f54e563788796310f4
Alexander Hostrup <alexander.hostrup@gmail.com>
2024-02-27 20:35:41 +0100

    Added interfaces

---
+++ stats.py
@@ -0,0 +1,7 @@
+class stats:
+    def __init__(self, health, vit, str, int, agi):
+        self.health = health
+        self.vitality = vit
+        self.strength = str
+        self.inteligence = int
+        self.agility = agi
