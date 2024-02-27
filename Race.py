7720238ec355c4fba2b4ce93025edad69101ef62
Alex <alexander.hostrup@gmail.com>
2024-02-27 14:07:00 +0100

    Added race selection

--- Race.py
+++ Race.py
@@ -1,29 +1,29 @@
 class race:
-    def __init__(self, description, vitMod, strMod, magMod, agiMod):
+    def __init__(self, name, description, vitMod, strMod, magMod, agiMod):
+        self.name = name
         self.description = description
         self.vitMod = vitMod
         self.strMod = strMod
         self.magMod = magMod
         self.agiMod = agiMod
 
-humanDescription = """
-Humans: With their ambition and resilience, Humans have built mighty kingdoms and sprawling cities.
+races = {}
+
+humanDescription = """Humans: With their ambition and resilience, Humans have built mighty kingdoms and sprawling cities.
 They are skilled in the arts of war and diplomacy, often at the forefront of facing external threats.
-
 """
-human = race(humanDescription, 1, 1, 1, 1)
+human = race("human", humanDescription, 1, 1, 1, 1)
+races[human.name] = human
 
-elfDescription = """
-Elves: Gifted with an innate connection to nature and magic, Elves are the stewards of the ancient forests.
+elfDescription = """Elves: Gifted with an innate connection to nature and magic, Elves are the stewards of the ancient forests.
 Their agility, grace, and mastery over the arcane arts make them formidable allies against the encroaching darkness.
-
 """
-elf = race(elfDescription, 0, 0, 2, 2)
+elf = race("elf", elfDescription, 0, 0, 2, 2)
+races[elf.name] = elf
 
-
-dwarfDescription = """
-Dwarves: Deep within the mountains, the Dwarves have crafted grand underground cities filled with intricate tunnels and majestic halls.
+dwarfDescription = """Dwarves: Deep within the mountains, the Dwarves have crafted grand underground cities filled with intricate tunnels and majestic halls.
 Masters of craftsmanship and metallurgy, Dwarves provide the strength and resilience needed for the world's defense.
 """
-dwarf = race(dwarfDescription, 2, 2, 0, 0)
+dwarf = race("dwarf", dwarfDescription, 2, 2, 0, 0)
+races[dwarf.name] = dwarf
 
--- main.py
+++ main.py
@@ -21,34 +21,55 @@ def startAnswer(answer):
         print("You have to be ready")
     start = input("are you ready for an adventure?")
 
-class Character:
-    def __init__ (self, name, race, RPGClass):
+""" class Character:
+    def __init__ (self, name, race, rpgClass):
         self.name = name
         self.race = race
-        self.RPGClass = RPGClass
+        self.rpgClass = rpgClass
 
 
-def __str(self):
-    return (f"Charecter: {self.name}\nRace: {self.race}\nRPGClass: {self.RPGClass}")
+    def __str(self):
+        return (f"Charecter: {self.name}\nRace: {self.race}\nRPGClass: {self.rpgClass}") """
 
 def create_Character():
+    os.system('cls')
     print("Welcome to Character creation!")
     name = input("Enter your name: ")
+    os.system('cls')
     while True:
         print("Races avallable: ")
-        for race_name in RPGClass.classes:
-            print(race_name)
-            race = input(f"Enter your race: [Dwarf, Human or Elf] ")
-    RPGClass = input("Enter your Class: [Warrior, Mage or Artcher] ")
+        for race_name in Race.races:
+            print(race_name.capitalize())
+            
+        race = input(f"Enter your race: [Dwarf, Human or Elf] ").lower()
+        os.system('cls')
 
+        raceChoice = Race.races[race]
 
+        print(f"""Description: {raceChoice.description}
+{raceChoice.name.capitalize()} has the following stats
+HP: {10 + raceChoice.vitMod}
+Strength: {1 + raceChoice.strMod}
+Inteligence: {1 + raceChoice.magMod}
+Agility: {1 + raceChoice.agiMod}""")
+        
+        input("Press enter to continue")
+        os.system('cls')
 
-    player = character.character("player", name, race, RPGClass)
-    return player
+        print("Classes available")
+        for class_name in RPGClass.classes:
+            print(class_name.capitalize())
+            
+        rpgClass = input("Enter your Class: [Warrior, Mage or Artcher] ")
 
+        player = character.character("player", name, race, rpgClass)
+        return player
+
+os.system('cls')
 print(text)
 
 name = input("What is your name? ")
+print()
 print(f"Nice to meeet you {name}")
 start = input("Are you ready for the adventure if your life? ")
 
