0667148111e440eb3a1dca0aee2ae6dca76424f9
poro246 <seb.zzxzz@gmail.com>
2024-02-27 11:17:36 +0100

    done maybe

--- Race.py
+++ Race.py
@@ -4,11 +4,12 @@ race_info = {
         'stats': {'vitality': 1, 'strength': 1, 'magic': 1, 'agility': 1},
     },
     'Elf': {
-        'description': 'A mystical folk from the deep forest, has great magical attributes and are highly adept.',
+        'description': 'A mystical folk from the deep forest, has great magical attributes and are highly adapt.',
         'stats': {'vitality': 0, 'strength': 0, 'magic': 2, 'agility': 2},
     },
     'Dwarf': {
-        'description': 'Warriors are brave fighters adept with heavy weapons and armor. They have high health and strength.',
+        'description': 'Proud and brave fighters adept with heavy weapons. They have high health and strength.',
         'stats': {'vitality': 2, 'strength': 2, 'magic': 0, 'agility': 0},
     },
 }
+
