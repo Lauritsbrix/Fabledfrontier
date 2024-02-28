fd40113bad8ece5f49d5469cbf3caf9d87bb4eb6
Alex <alexander.hostrup@gmail.com>
2024-02-28 10:27:13 +0100

    Started gui

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+GAME_WIDTH = 640
+GAME_HEIGHT  = 320
+
+root = Tk()
+root.geometry(str(GAME_WIDTH) + 'x' + str(GAME_HEIGHT))
+# frame = ttk.Frame(root, width=GAME_WIDTH, height=GAME_HEIGHT)
+
+
+root.mainloop()
