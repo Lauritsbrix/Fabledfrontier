8445a26d49b568c88c9ac56c8907cc459951b500
Alex <alexander.hostrup@gmail.com>
2024-02-28 11:10:11 +0100

    Added lore and started gui

---
+++ lore.py
@@ -0,0 +1,5 @@
+introText = """In the realm of Luminos, a land shrouded in the mists of ancient magic and surrounded by untamed wilderness,
+three distinct races coexisted for centuries – Humans, Elves, and Dwarves.
+They have shared the blessings of their fertile lands, harnessed the power of arcane forces, and built great cities that touched the skies.
+"""
+
