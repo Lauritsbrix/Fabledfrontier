68d8ecbaea5e9c26a95ad502cf00df5ed06352d9
Mathias Christiansen <Smatten2264@outlook.com>
2024-02-28 09:34:21 +0100

    combat system

---
+++ combat.py
@@ -0,0 +1,38 @@
+import random
+from random import randint
+from character import character
+from stats import stats
+import settings
+settings.init()
+
+pers = character("player", "halo", "human", "mage")
+
+orcStats = stats (5)
+orc = character("enemy", "orc", "orc", "warrior", orcStats)
+
+def main(player, enemy):
+    playerHealth = player.stats.health
+    enemyHealth = enemy.stats.health
+    print(f"player has {playerHealth}, enemy has {enemyHealth}")
+
+print("an awesome battle is about to start")
+    
+
+
+def attack(attacker, deffender): 
+    print(f"orc has {deffender.stats.health} hp")
+    damage = attacker.stats.strength
+    deffender.stats.health -= damage
+    print(f"orc has {deffender.stats.health} hp")
+
+
+
+
+attack(pers, orc)
+main(pers, orc)
+
+
+
+
+
+
