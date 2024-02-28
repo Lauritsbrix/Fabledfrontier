791f911ed5c77d417506126c28fe549a9e0c5f3c
Laurits <you@example.com>
2024-02-28 11:14:58 +0100

    commit

--- OpeningGUI.py
+++ OpeningGUI.py
@@ -6,7 +6,7 @@ from tkinter.ttk import *
 root = tk.Tk()
 root.title("Luminos")
 
-magePhoto = PhotoImage(file = r"wizard.png")
+magePhoto = PhotoImage(file = r"images/wizard.png")
 
 
 # Funktion, der udføres ved at klikke på knappen
@@ -22,8 +22,6 @@ def button_click():
     mageButton.pack(side=TOP)
 
 
-button = tk.Button(text="Continue", command=button_click)
-button.pack(pady=20)
 
 # Opret en label (tekstfelt)
 label = tk.Label(root, text="""In the realm of Luminos, a land shrouded in the mists of ancient magic and surrounded by untamed wilderness,
@@ -32,8 +30,7 @@ They have shared the blessings of their fertile lands, harnessed the power of ar
 """, font=("Helvetica", 16))
 label.pack(pady=10)
 
-# Opret en knap
-button = tk.Button(root, text="Continue", command=button_click)
+button = tk.Button(text="Continue", command=button_click)
 button.pack(pady=20)
 
 # Start hovedløkken for at vise vinduet
